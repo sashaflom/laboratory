6e4882996b9a02190e2d9384e0dde915e1430911
Oleksandra Flomboim <sflomboim@gmail.com>
2026-02-07 21:59:41 +0200

    Add additional logic in methods when teachers list don't have teachers

--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -69,39 +69,53 @@ public class Repository {
         if(makingSure==1){
             System.out.println("Список наявних викладачів:");
             showAllTeachers();
-            System.out.println("Введіть ID викладача, якого хочете видалити: ");
-            scanner.nextLine();
-            String id = scanner.nextLine();
-            while(id.length() != 5){
-                System.out.println("Не може бути такої довжини, введіть 5 знаків: ");
-                id = scanner.nextLine();
-            }
-            Optional<Teacher> maybeTeacher = findTeacherByID(id);
-            if (maybeTeacher.isPresent()){
-                Teacher foundTeacher = maybeTeacher.get();
-                teachers.remove(foundTeacher);
-                System.out.println("Ви успішно видалили викладача. Оновлений список наявних викладачів:");
-                showAllTeachers();
+            if(teachers.size()!=0){
+                System.out.println("Введіть ID викладача, якого хочете видалити: ");
+                scanner.nextLine();
+                String id = scanner.nextLine();
+                while(id.length() != 5){
+                    System.out.println("Не може бути такої довжини, введіть 5 знаків: ");
+                    id = scanner.nextLine();
+                }
+                Optional<Teacher> maybeTeacher = findTeacherByID(id);
+                if (maybeTeacher.isPresent()){
+                    Teacher foundTeacher = maybeTeacher.get();
+                    teachers.remove(foundTeacher);
+                    System.out.println("Ви успішно видалили викладача. Оновлений список наявних викладачів:");
+                    showAllTeachers();
+                }else{
+                    System.out.println("Немає викладача з таким ID.");
+                }
             }else{
-                System.out.println("Немає викладача з таким ID.");
+                System.out.println("Не створено жодного викладача, отже ви не можете нікого видалити");
             }
+
         }
     }
 
     public void showAllTeachers(){
-        for (Teacher teacher : teachers) {
-            System.out.println(teacher);
+        if (teachers.size()!=0){
+            for (Teacher teacher : teachers) {
+                System.out.println(teacher);
+            }
+        }else{
+            System.out.println("Поки що немає жодного викладача");
         }
     }
 
     public Optional<Teacher> findTeacherByID(String id){
         Optional<Teacher> foundTeacher = Optional.empty();
-        for (Teacher teacher : teachers) {
-            if(teacher.getId().equals(id)){
-                foundTeacher = Optional.of(teacher);
-                break;
+        if(teachers.size()!=0){
+            for (Teacher teacher : teachers) {
+                if(teacher.getId().equals(id)){
+                    foundTeacher = Optional.of(teacher);
+                    break;
+                }
             }
+        }else{
+            System.out.println("Не створено жодного викладача, отже ви не можете знайти");
         }
+
         return foundTeacher;
     }
 
@@ -124,87 +138,92 @@ public class Repository {
         if(makingSure==1) {
             System.out.println("Список наявних викладачів:");
             showAllTeachers();
-            System.out.println("Введіть ID викладача, якого хочете редагувати: ");
-            scanner.nextLine();
-            String id = scanner.nextLine();
-            while (id.length() != 5) {
-                System.out.println("Не може бути такої довжини, введіть 5 знаків: ");
-                id = scanner.nextLine();
-            }
-            Optional<Teacher> maybeTeacher = findTeacherByID(id);
-            if (maybeTeacher.isPresent()){
-                Teacher foundTeacher = maybeTeacher.get();
-                while(true){
-                    System.out.println("Виберіть, що ви хочете змінити:" +
-                            "\n1 - прізвище" +
-                            "\n2 - ім'я" +
-                            "\n3 - побатькові" +
-                            "\n4 - дату народження" +
-                            "\n5 - email" +
-                            "\n6 - номер телефону" +
-                            "\n7 - посаду" +
-                            "\n8 - науковий ступінь" +
-                            "\n9 - вчене звання" +
-                            "\n10 - дату прийняття на роботу" +
-                            "\n11 - ставку" +
-                            "\n0 - вийти на рівень вище");
-                    int whatToChange;
-                    while (!scanner.hasNextInt()) {
-                        String input = scanner.next();
-                        System.out.println("Немає такої опції, введіть число відповідно до інструкції: ");
-                    }
-                    whatToChange = scanner.nextInt();
-                    while(whatToChange<0 || whatToChange>11){
-                        System.out.println("Немає такої опції, введіть число відповідно до інструкції: ");
+            if(teachers.size()!=0){
+                System.out.println("Введіть ID викладача, якого хочете редагувати: ");
+                scanner.nextLine();
+                String id = scanner.nextLine();
+                while (id.length() != 5) {
+                    System.out.println("Не може бути такої довжини, введіть 5 знаків: ");
+                    id = scanner.nextLine();
+                }
+                Optional<Teacher> maybeTeacher = findTeacherByID(id);
+                if (maybeTeacher.isPresent()){
+                    Teacher foundTeacher = maybeTeacher.get();
+                    while(true){
+                        System.out.println("Виберіть, що ви хочете змінити:" +
+                                "\n1 - прізвище" +
+                                "\n2 - ім'я" +
+                                "\n3 - побатькові" +
+                                "\n4 - дату народження" +
+                                "\n5 - email" +
+                                "\n6 - номер телефону" +
+                                "\n7 - посаду" +
+                                "\n8 - науковий ступінь" +
+                                "\n9 - вчене звання" +
+                                "\n10 - дату прийняття на роботу" +
+                                "\n11 - ставку" +
+                                "\n0 - вийти на рівень вище");
+                        int whatToChange;
                         while (!scanner.hasNextInt()) {
                             String input = scanner.next();
                             System.out.println("Немає такої опції, введіть число відповідно до інструкції: ");
                         }
                         whatToChange = scanner.nextInt();
-                    }
+                        while(whatToChange<0 || whatToChange>11){
+                            System.out.println("Немає такої опції, введіть число відповідно до інструкції: ");
+                            while (!scanner.hasNextInt()) {
+                                String input = scanner.next();
+                                System.out.println("Немає такої опції, введіть число відповідно до інструкції: ");
+                            }
+                            whatToChange = scanner.nextInt();
+                        }
 
-                    // last name to change
-                    if (whatToChange==1){
-                        foundTeacher.setLastname(Teacher.lastNameValidation());
-                        // first name to change
-                    }else if(whatToChange==2){
-                        foundTeacher.setFirstname(Teacher.firstNameValidation());
-                        // patronymic name to change
-                    }else if (whatToChange==3){
-                        foundTeacher.setPatronymic(Teacher.patronymicValidation());
-                        // birth date to change
-                    }else if(whatToChange==4){
-                        foundTeacher.setBirthDate(Teacher.birthDateValidation());
-                        // email to change
-                    }else if(whatToChange==5){
-                        foundTeacher.setEmail(Teacher.emailValidation());
-                        // phone number to change
-                    }else if (whatToChange==6){
-                        foundTeacher.setPhoneNumber(Teacher.phoneNumberValidation());
-                        // position to change
-                    }else if(whatToChange==7){
-                        foundTeacher.setPosition(Teacher.positionValidation());
-                        // academic degree was chosen
-                    }else if(whatToChange==8){
-                        foundTeacher.setAcademicDegree(Teacher.academicDegreeValidation());
-                        // academic title to change
-                    }else if (whatToChange==9){
-                        foundTeacher.setAcademicTitle(Teacher.academicTitleValidation());
-                        // hire date to change
-                    }else if(whatToChange==10){
-                        foundTeacher.setHireDate(Teacher.hireDateValidation());
-                        // workload to change
-                    }else if(whatToChange==11){
-                        foundTeacher.setWorkload(Teacher.workloadValidation());
-                        // exit
-                    }else if(whatToChange==0){
-                        break;
+                        // last name to change
+                        if (whatToChange==1){
+                            foundTeacher.setLastname(Teacher.lastNameValidation());
+                            // first name to change
+                        }else if(whatToChange==2){
+                            foundTeacher.setFirstname(Teacher.firstNameValidation());
+                            // patronymic name to change
+                        }else if (whatToChange==3){
+                            foundTeacher.setPatronymic(Teacher.patronymicValidation());
+                            // birth date to change
+                        }else if(whatToChange==4){
+                            foundTeacher.setBirthDate(Teacher.birthDateValidation());
+                            // email to change
+                        }else if(whatToChange==5){
+                            foundTeacher.setEmail(Teacher.emailValidation());
+                            // phone number to change
+                        }else if (whatToChange==6){
+                            foundTeacher.setPhoneNumber(Teacher.phoneNumberValidation());
+                            // position to change
+                        }else if(whatToChange==7){
+                            foundTeacher.setPosition(Teacher.positionValidation());
+                            // academic degree was chosen
+                        }else if(whatToChange==8){
+                            foundTeacher.setAcademicDegree(Teacher.academicDegreeValidation());
+                            // academic title to change
+                        }else if (whatToChange==9){
+                            foundTeacher.setAcademicTitle(Teacher.academicTitleValidation());
+                            // hire date to change
+                        }else if(whatToChange==10){
+                            foundTeacher.setHireDate(Teacher.hireDateValidation());
+                            // workload to change
+                        }else if(whatToChange==11){
+                            foundTeacher.setWorkload(Teacher.workloadValidation());
+                            // exit
+                        }else if(whatToChange==0){
+                            break;
+                        }
+                        System.out.println("Ви успішно змінили викладача: " + foundTeacher);
                     }
-                    System.out.println("Ви успішно змінили викладача: " + foundTeacher);
+                }else{
+                    System.out.println("Немає викладача з таким ID.");
                 }
             }else{
-                System.out.println("Немає викладача з таким ID.");
+                System.out.println("Не створено жодного викладача, отже ви не можете змінити");
             }
+
         }
     }
 
