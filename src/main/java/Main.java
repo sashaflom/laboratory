02d2214df88b7c04f5036a8f079dc52f18dedb3c
Oleksandra Flomboim <sflomboim@gmail.com>
2026-02-07 16:44:37 +0200

    Add everything in Repository class, add basic selection structure in Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,80 @@
+import domain.University;
+import domain.Department;
+import domain.Faculty;
+import domain.Student;
+import domain.Teacher;
+
+import java.util.Scanner;
+
 public class Main {
 
+    private static University kyivMohylaAcademy;
+    private static Scanner scanner = new Scanner(System.in);
+
     public static void main(String[] args){
+        kyivMohylaAcademy = new University("Національний університет 'Києво-Могилянська академія'",
+                "НаУКМА", "Київ", "вулиця Григорія Сковороди, 2, Київ, 04655");
+        System.out.println("Вітаю в інформаційній системі Києво-Могилянської академії!");
+        System.out.println("\nСтворено:" + kyivMohylaAcademy);
+
+        while (true){
+            System.out.println("Виберіть, що хочете зробити:" +
+                    "\n1 - операції керування даними" +
+                    "\n2 - пошук" +
+                    "\n3 - звіти" +
+                    "\n0 - закінчити");
+            int whatToDo = scanner.nextInt();
+            //invalid input validation
+            while(whatToDo<0 || whatToDo>3){
+                System.out.println("Немає такої опції, введіть число відповідно до інструкції: ");
+                whatToDo = scanner.nextInt();
+            }
+
+            // data management operations was chosen
+            if (whatToDo==1){
+                while(true){
+                    System.out.println("Виберіть, з чим хочете працювати: " +
+                            "\n1 - факультет" +
+                            "\n2 - кафедра" +
+                            "\n3 - студент" +
+                            "\n4 - викладач" +
+                            "\n0 - вийти на рівень вище");
+                    int whatToWorkWith = scanner.nextInt();
+                    //invalid input validation
+                    while(whatToWorkWith<0 || whatToWorkWith>4){
+                        System.out.println("Немає такої опції, введіть число відповідно до інструкції: ");
+                        whatToWorkWith = scanner.nextInt();
+                    }
+
+                    // faculty was chosen
+                    if (whatToWorkWith==1){
+
+                        // department was chosen
+                    }else if(whatToWorkWith==2){
+
+                        // student was chosen
+                    }else if (whatToWorkWith==3){
+
+                        // teacher was chosen
+                    }else if(whatToWorkWith==4){
+
+                        // exit was chosen
+                    }else if(whatToWorkWith==0){
+                        break;
+                    }
+                }
+                // search was chosen
+            }else if(whatToDo==2){
+
+                // reports was chosen
+            }else if (whatToDo==3){
+
+                // exit was chosen
+            }else if(whatToDo==0){
+                System.out.println("Дякую, що прийшли!");
+                break;
+            }
+        }
 
     }
 
--- src/main/java/repository/Repository.java
+++ src/main/java/repository/Repository.java
@@ -1,4 +1,41 @@
 package repository;
 
+import domain.Department;
+import domain.Faculty;
+import domain.Student;
+import domain.Teacher;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Repository {
+
+    // static class variable that will store a reference to a single instance of the class
+    private static Repository instance = null;
+
+    private List<Faculty> faculties = new ArrayList<>();
+    private List<Department> departments = new ArrayList<>();
+    private List<Student> students = new ArrayList<>();
+    private List<Teacher> teachers = new ArrayList<>();
+
+    // the constructor is private so that it is impossible to create an instance of the class anywhere outside
+    private Repository(){
+
+    }
+
+    // a static class method that should return a reference to a single instance of the class
+    public static Repository getInstance(){
+        // if it's null, then this is the first call to the method and you need to create a new instance of the class
+        if (instance == null){
+            // create and write it to a static class variable
+            instance = new Repository();
+            /*
+            after that, in subsequent calls to getInstance we will never create a new
+            instance, but will always return the one created in the very first call
+             */
+        }
+        // return value
+        return instance;
+    }
+
 }
