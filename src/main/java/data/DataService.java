e0028f153398799c4a309916c6332e436b6e2521
Oleksandra Flomboim <sflomboim@gmail.com>
2026-04-04 13:16:59 +0300

    add DataService with templates methods

---
+++ src/main/java/data/DataService.java
@@ -0,0 +1,17 @@
+package data;
+
+import java.nio.file.Path;
+
+public class DataService {
+
+    private final Path in = Path.of("data", "uniData.json");
+
+    public void saveData(){
+
+    }
+
+    public void loadData(){
+
+    }
+
+}
