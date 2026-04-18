f76b8af99a7a1ad62ca74fa6b20c704ebda3e75c
Oleksandra Flomboim <sflomboim@gmail.com>
2026-04-18 20:32:46 +0300

    add logging in PersonValidator

--- src/main/java/validators/PersonValidator.java
+++ src/main/java/validators/PersonValidator.java
@@ -1,8 +1,11 @@
 package validators;
 
+import data.DataService;
 import domain.Student;
 import domain.Teacher;
 import exceptions.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import repositories.StudentRepository;
 import repositories.TeacherRepository;
 
@@ -14,6 +17,7 @@ public class PersonValidator {
 
     private static TeacherRepository repositoryT = TeacherRepository.getInstance();
     private static StudentRepository repositoryS = StudentRepository.getInstance();
+    private static final Logger logger = LoggerFactory.getLogger(PersonValidator.class);
 
     public static void isIdValid(String id) throws DuplicateIdException {
         Optional<Teacher> maybeTeacher = repositoryT.findById(id);
@@ -30,8 +34,10 @@ public class PersonValidator {
     public static boolean isBirthDateValid(int year, int month, int day){
         try{
             LocalDate.of(year, month, day);
+            logger.info("Дата {} валідна", LocalDate.of(year, month, day));
             return true;
         } catch (DateTimeException e){
+            logger.error("Помилка, невалідна дата для чисел рік {} місяць {} день {}: {}", year, month, day, e.getMessage());
             return false;
         }
     }
