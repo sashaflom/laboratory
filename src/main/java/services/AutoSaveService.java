32325f5b3d94eaee3bf017531267b8f79154440e
Oleksandra Flomboim <sflomboim@gmail.com>
2026-04-18 20:05:49 +0300

    add logging in AutoSaveService

--- src/main/java/services/AutoSaveService.java
+++ src/main/java/services/AutoSaveService.java
@@ -2,19 +2,27 @@ package services;
 
 import data.DataService;
 
+import java.nio.file.Path;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class AutoSaveService {
 
     private final ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+    private final static Path in = Path.of("src", "main", "java", "data", "uniData.json");
+    private static final Logger logger = LoggerFactory.getLogger(AutoSaveService.class);
 
     public void start() {
         scheduler.scheduleAtFixedRate(() -> {
             try {
                 DataService.saveData();
+                logger.info("Автозбереження в файл {} успішнe.", in);
             } catch (Exception e) {
+                logger.error("Помилка автозбереження у файл {}: {}.", in, e.getMessage());
                 System.out.println("\n[AUTO-SAVE] Помилка у фоновому потоці автозбереження.");
                 e.printStackTrace();
             }
@@ -27,7 +35,9 @@ public class AutoSaveService {
             if (!scheduler.awaitTermination(3, TimeUnit.SECONDS)) {
                 scheduler.shutdownNow();
             }
+            logger.info("Закриття автозбереження успішне.");
         } catch (InterruptedException e) {
+            logger.error("Помилка закриття автозбереження: {}", e.getMessage());
             scheduler.shutdownNow();
             Thread.currentThread().interrupt();
         }
